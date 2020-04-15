665ba968e5b3db04a6c54d2f44c10ba8a879ad40
vaibhav-trq <55691258+vaibhav-trq@users.noreply.github.com>
2020-04-15 08:25:38 +0000

    Updating all questions

--- fix-code-0/main.cpp
+++ fix-code-0/main.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main() {
-  int x=4;
-  int y=5;
-  int z=x*y;
-  cout<<"The value of z is: ";
-  cout<<z;
-  cout<<endl;
+  int x = 4;
+  int y = 5;
+  int z = x * y;
+  cout << "The value of z is: ";
+  cout << z;
+  cout << endl;
 }
