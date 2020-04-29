607a782bc7d39015ea5766c1cd1f83084e690fb7
hellovai <2448742+hellovai@users.noreply.github.com>
2020-04-29 07:32:04 +0000

    Saving progress for all questions.

--- fix-code-0/main.cpp
+++ fix-code-0/main.cpp
@@ -3,10 +3,7 @@
 using namespace std;
 
 int main() {
-  int x=4;
-  int y=5;
-  int z=x*y;
-  cout<<"The value of z is: ";
-  cout<<z;
-  cout<<endl;
+  while(true) {
+    cout << "Hello!" << endl;
+  }
 }
