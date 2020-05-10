b6bf3c91b50526a47bebbebcd64e2c2369524bf2
vaibhav-trq <55691258+vaibhav-trq@users.noreply.github.com>
2020-05-10 22:59:38 +0000

    Saving progress for all questions.

--- fix-code-0/main.cpp
+++ fix-code-0/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <string>
-using namespace 
+using namespace std;
 
 int main() {
-  int x = 4;
-  int y = 5;
-  int z = x * y;
-  cout << "The value of z is: ";
-  cout << z;
-  cout << endl;
+  int x=4;
+  int y=5;
+  int z=x*y;
+  cout<<"The value of z is: ";
+  cout<<z;
+  cout<<endl;
 }
